fb3e40bb39a09295b5c902ffb16648b8fba69e9b
AdrianMitroiu <adrian.mitroiu@unity-solutions.ro>
2019-06-27 12:40:54 +0300

    Repository with Magento Payment Sample

--- registration.php
+++ registration.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Twispay_Payments',
+    'Magento_SamplePaymentGateway',
     __DIR__
 );
